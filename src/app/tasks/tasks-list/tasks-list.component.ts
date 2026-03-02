28577d55bcfc1c160e4f87aef8cc83fbb955dd9b
jovan.petrovic <jovanloki@gmail.com>
2026-03-02 11:50:53 +0100

    Implemented filtering of tasks using computed signal

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksService } from '../tasks.service';
@@ -19,8 +19,28 @@ export class TasksListComponent {
    * After injecting the service, tasks is a writable signal. If we want to make it read-only, we need to protect it with a getter
    * and return the value of the signal. This way, we can only read the value of the signal, but we cannot modify it directly.
    * We can only modify it through the methods provided by the service.
+   *
+   * We change tasks to computed() signal to be able to filter the tasks based on the selected filter. The computed() signal will
+   * automatically re-evaluate whenever the selectedFilter or the tasks signal changes.
    */
-  tasks = this.tasksService.allTasks;
+  tasks = computed(() => {
+    switch (this.selectedFilter()) {
+      case 'open':
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === 'OPEN');
+      case 'in-progress':
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === 'IN_PROGRESS');
+      case 'done':
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === 'DONE');
+      default:
+        return this.tasksService.allTasks();
+    }
+  });
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, signal } from '@angular/core';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable({
   providedIn: 'root',
@@ -23,5 +23,13 @@ export class TasksService {
 
     // We should not mutate the old array, but create a new one with the new task added
     this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
+
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task,
+      ),
+    );
   }
 }
