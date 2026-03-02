748d21329e6d63a9bb3ab075a22d276fb31c8618
jovan.petrovic <jovanloki@gmail.com>
2026-03-02 14:43:14 +0100

    Added useful tips on common types of injectors and how should one use them.

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,6 +1,27 @@
 import { Injectable, signal } from '@angular/core';
 import { Task, TaskStatus } from './task.model';
 
+/**
+ * There are different ways to provide a service in Angular. The most common way is to use the @Injectable decorator with the providedIn
+ * property set to 'root'. This means that the service will be available application-wide and there will be only one instance of it (singleton).
+ *
+ * Another way to provide a service is to add it to the providers array of a component. This means that the service will be available
+ * only within that component and its children, and there will be a new instance of the service for each component that provides it.
+ *
+ * In this case, we want the TasksService to be available application-wide, so we use the providedIn: 'root' option in the @Injectable decorator.
+ * This way, we can inject the TasksService into any component that needs it, and we will always get the same instance of the service.
+ *
+ * Different injectors in Angular have different lifecycles. The root injector is created when the application starts and is destroyed when
+ * the application is destroyed. The component injector is created when the component is created and is destroyed when the component is destroyed.
+ * The module injector is created when the module is loaded and is destroyed when the module is unloaded. The service injector is created when
+ * the service is created and is destroyed when the service is destroyed.
+ *
+ * @Injectable uses application root (Environment injector)
+ * Module injector is used by providing the service named in the providers array of a module.
+ * Platform Environment injector is used by providing the service named in the providers array of a platform. (Add an object to the providers
+ * array of the platform when bootstrapping the application).
+ */
+
 @Injectable({
   providedIn: 'root',
 })
